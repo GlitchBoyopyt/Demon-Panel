23dd8fdb81f9bd188dd6733742650bbfecbb642f
M.R Glitch <162595849+GlitchBoyopyt@users.noreply.github.com>
2025-03-09 23:34:31 +0530

    Create App.vue

---
+++ App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <router-view v-slot="{ Component }">
+      <transition name="fade" mode="out-in">
+        <component :is="Component" />
+      </transition>
+    </router-view>
+  </div>
+</template>
+
+<style>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+</style>
