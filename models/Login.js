6f6b5cc92c319d4eb91ccb627106d88c3f698af9
M.R Glitch <162595849+GlitchBoyopyt@users.noreply.github.com>
2025-03-09 19:44:35 +0530

    Create Login.js

---
+++ models/Login.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post("http://localhost:5000/api/auth/login", { username, password });
+      localStorage.setItem("token", res.data.token);
+      window.location.href = "/dashboard";
+    } catch (error) {
+      alert("Invalid credentials");
+    }
+  };
+
+  return (
+    <div>
+      <input type="text" placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
+      <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+};
+
+export default Login;
