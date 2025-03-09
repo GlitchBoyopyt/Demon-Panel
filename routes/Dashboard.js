e067d7d313f8f50466dcc3418a4cceb9b5c59b65
M.R Glitch <162595849+GlitchBoyopyt@users.noreply.github.com>
2025-03-09 21:38:51 +0530

    Update Dashboard.js

--- routes/Dashboard.js
+++ routes/Dashboard.js
@@ -1,32 +1,13 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-
-const Dashboard = () => {
-  const [servers, setServers] = useState([]);
-
-  useEffect(() => {
-    axios.get("http://localhost:5000/api/servers", {
-      headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
-    })
-      .then(res => setServers(res.data))
-      .catch(err => console.error(err));
-  }, []);
-
-  return (
-    <div>
-      <h1>Server Dashboard</h1>
-      <ul>
-        {servers.map(server => (
-          <li key={server._id}>
-            {server.name} - {server.status}
-            <button onClick={() => axios.post(`http://localhost:5000/api/servers/start/${server._id}`)}>Start</button>
-            <button onClick={() => axios.post(`http://localhost:5000/api/servers/stop/${server._id}`)}>Stop</button>
-            <button onClick={() => axios.post(`http://localhost:5000/api/servers/restart/${server._id}`)}>Restart</button>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+const handleServerAction = async (serverId, action) => {
+  try {
+    await axios.post(`http://localhost:5000/api/pterodactyl/${action}/${serverId}`);
+    alert(`Server ${action}ed successfully!`);
+  } catch (error) {
+    alert("Failed to perform action!");
+  }
 };
 
-export default Dashboard;
+// Usage in UI
+<button onClick={() => handleServerAction(server._id, "start")}>Start</button>
+<button onClick={() => handleServerAction(server._id, "stop")}>Stop</button>
+<button onClick={() => handleServerAction(server._id, "restart")}>Restart</button>
